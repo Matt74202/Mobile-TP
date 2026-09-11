5693fa1f03295a13bc6404442f02f9fb4ca8da05
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2026-09-11 17:39:42 +0300

    Initial commit - Project structure

---
+++ app/src/main/java/mg/itu/mizaha/MainActivity.kt
@@ -0,0 +1,23 @@
+package mg.itu.mizaha
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            TestScreen()
+        }
+    }
+}
+
+@Composable
+fun TestScreen() {
+    Text("Mon projet fonctionne !")
+}
