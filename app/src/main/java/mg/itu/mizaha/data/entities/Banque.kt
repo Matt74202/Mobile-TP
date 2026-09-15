27e1bbe05e539243adc4a0c0888362e56433426c
Matthew <matthewbogosy@gmail.com>
2026-09-15 18:18:52 +0300

    adding functionnalities to Banque

--- app/src/main/java/mg/itu/mizaha/data/entities/Banque.kt
+++ app/src/main/java/mg/itu/mizaha/data/entities/Banque.kt
@@ -1,8 +1,14 @@
 package mg.itu.mizaha.data.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "banques")
 data class Banque(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val nom: String,
     val enseigne: String,
-    val lieu: String
+    val lieu: String,
+    val latitude: Double,
+    val longitude: Double
 )
