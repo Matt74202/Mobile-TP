40fb1bbb3ad2149c782397e4d8576e5c2cd4ce7a
Matthew <matthewbogosy@gmail.com>
2026-09-15 14:43:19 +0300

    adding modifs to Hotel entity

--- app/src/main/java/mg/itu/mizaha/data/entities/Hotel.kt
+++ app/src/main/java/mg/itu/mizaha/data/entities/Hotel.kt
@@ -1,9 +1,15 @@
 package mg.itu.mizaha.data.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "hotels")
 data class Hotel(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val nom: String,
     val lieu: String,
+    val latitude: Double,
+    val longitude: Double,
     val contact: String,
     val siteWeb: String?,
     val imageRes: Int
